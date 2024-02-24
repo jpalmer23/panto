2739385d7b62b4c929e339aa75e0faa9a3ee4f61
JPalmerr23 <dvorzhak21@gmail.com>
2024-02-24 02:56:04 +0100

    final commit

--- js/main.js
+++ js/main.js
@@ -30,20 +30,27 @@ const swiper = new Swiper('.swiper', {
     freeMode: true,
     slidesPerView: 4,
     spaceBetween: 42,
-    // breakpoints: {
-    //     640: {
-    //         slidesPerView: 2,
-    //         spaceBetween: 20,
-    //     },
-    //     768: {
-    //         slidesPerView: 4,
-    //         spaceBetween: 40,
-    //     },
-    //     1024: {
-    //         slidesPerView: 5,
-    //         spaceBetween: 50,
-    //     },
-    // },
+    breakpoints: {
+        320: {
+            slidesPerView: 1,
+        },
+        600: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+        920: {
+            slidesPerView: 3,
+            spaceBetween: 30,
+        },
+        992: {
+            slidesPerView: 3,
+            spaceBetween: 42,
+        },
+        1230: {
+            slidesPerView: 4,
+            spaceBetween: 42,
+        },
+    },
 
 
     // Navigation arrows
@@ -51,4 +58,56 @@ const swiper = new Swiper('.swiper', {
       nextEl: '#sliderNext',
       prevEl: '#sliderPrev',
     },
-  });
+});
+
+//tabs
+
+const tabsBtn = document.querySelectorAll('[data-tab]');
+const tabsProducts = document.querySelectorAll('[data-tab-value]');
+for (let btn of tabsBtn) {
+    btn.addEventListener('click', function () {
+        for (let btn of tabsBtn) {
+            // console.log(this.dataset.tab);
+            btn.classList.remove('tab-controls__btn--active');
+        }
+        this.classList.add('tab-controls__btn--active');
+
+        for (let product of tabsProducts) {
+
+			console.log('Current tab:', this.dataset.tab);
+            if (this.dataset.tab === 'All') {
+                console.log(this.dataset.tab);
+				product.classList.remove('none');
+			} else {
+				if (product.dataset.tabValue === this.dataset.tab) {
+					product.classList.remove('none');
+				} else {
+					product.classList.add('none');
+				}
+			}
+		}
+        swiper.update();
+    });
+};
+
+const mobileNavOpenBtn = document.querySelector('.nav__btn');
+const mobileNav = document.querySelector('.mobile-nav-wrapper');
+const mobileNavCloseBtn = document.querySelector('.mobile-nav_btn-close');
+const mobileCart = document.querySelector('.cart');
+const furnitureDropDown = document.querySelector('.mobile-nav__link--dropdown');
+const furnitureMenu = document.querySelector('.mobile-sub-nav');
+mobileNavOpenBtn.addEventListener('click', function () {
+    mobileNav.classList.add('mobile-nav-wrapper--open');
+    mobileCart.classList.add('none');
+    mobileNavOpenBtn.classList.add('none');
+});
+mobileNavCloseBtn.addEventListener('click', function () {
+    mobileNav.classList.remove('mobile-nav-wrapper--open');
+    mobileCart.classList.remove('none');
+    mobileNavOpenBtn.classList.remove('none');
+});
+
+furnitureDropDown.onclick = function () {
+    furnitureMenu.classList.toggle('none');
+};
+
